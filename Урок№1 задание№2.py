43479d243541904a6ac1adc544b73c5143e1c546
SeregaLikhanov <113440195+SeregaLikhanov@users.noreply.github.com>
2023-09-16 21:01:35 +0300

    5

--- Урок№1 задание№2.py
+++ Урок№1 задание№2.py
@@ -4,7 +4,7 @@ homo2 = input("Vedite vtoruy stadiy razvitia cheloveka: ")
 homo3 = input("Vedite tretiy stadiy razvitia cheloveka: ") 
 homo4 = input("Vedite chetvortuy stadiy razvitia cheloveka: ")
 homo5 = input("Vedite poslednuy stadiy razvitia cheloveka: ")
-print(homo1, homo2, homo3,homo4, homo5, sep="=>")
+print(homo1, homo2, homo3, homo4, homo5, sep="=>")
 
 
 
---
+++ Урок№5 Задание1.py
@@ -0,0 +1,11 @@
+number = int(input())
+
+if (number >= 0) and (number % 2 == 0):
+    print("chetnoe polojitelnoe chislo")
+elif (number < 0) and (number % 2 != 0):
+    print("otritelnoe chislo nechetnoe")
+else:
+    print("nulevoe cheslo")
+
+
+
