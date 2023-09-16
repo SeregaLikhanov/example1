65611b16f5a5317ad5e937b1936e1a5b3282352b
SeregaLikhanov <113440195+SeregaLikhanov@users.noreply.github.com>
2023-09-16 20:13:17 +0300

    1

---
+++ Урок№1 задание№2.py
@@ -0,0 +1,10 @@
+# homo1 Australopithecus africanus, homo2 Homo habilis, homo3 Homo erectus, homo4 homo Neanderthalensis, homo5 Homo Sapiens Sapiens
+homo1 = input("Vedite pervuy stadiy razvitiya cheloveka: ")
+homo2 = input("Vedite vtoruy stadiy razvitia cheloveka: ")
+homo3 = input("Vedite tretiy stadiy razvitia cheloveka: ") 
+homo4 = input("Vedite chetvortuy stadiy razvitia cheloveka: ")
+homo5 = input("Vedite poslednuy stadiy razvitia cheloveka: ")
+print(homo1, homo2, homo3,homo4, homo5, sep="=>")
+
+
+
